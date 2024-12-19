9aef41903744c25fa0aee85310293e20b96e94e2
Chips Choking <52846426+choking-chips@users.noreply.github.com>
2024-12-19 16:46:09 +0800

    下落并判断

--- MDI-Match-3/script-judgement.js
+++ MDI-Match-3/script-judgement.js
@@ -1,8 +1,10 @@
+let changedIcons = [];
+
 function judge(icons) {
     let matchResult = false;
     icons.forEach(icon => { // 轮番判断所有参数给的值
         matchResult = match(check(icon), icon.numId) || matchResult;
-    }); // 现在需要将两个id临时换一下，不行再换回来，否则不好参与判断
+    });
     return matchResult;
 }
 
@@ -36,7 +38,7 @@ function match(matchNum, iconNumId) { // 匹配iconNumId所在的位置是否有
         if (matchNum[i] + matchNum[i + 1] >= 2) { // 如果这行/列能消除
             const ori = w ** (i / 2); // orientation，横着等于1，竖着等于w
             for (let j = iconNumId - matchNum[i] * ori; j <= iconNumId + matchNum[i + 1] * ori; j += ori) {
-                getIconByNumId(j).style.opacity = '0.2'; // 隐藏能连起来的
+                getIconByNumId(j).style.opacity = '0'; // 隐藏能连起来的
                 matchResult = true;
             }
         }
@@ -45,5 +47,43 @@ function match(matchNum, iconNumId) { // 匹配iconNumId所在的位置是否有
 }
 
 function fall() { // 下落判定
+    changedIcons = [];
+    for (let c = 1; c <= w; c++) { // c 为第几列
+        let cur1 = w * (h - 1) + c; // 指向最底层
+        while (cur1 > 0 && getIconByNumId(cur1).style.opacity != '0') { // 向上找空缺
+            cur1 -= w;
+        }
+        let cur2 = cur1;
+        while (cur2 > w) { // 向上找有的
+            cur2 -= w;
+            if (getIconByNumId(cur2).style.opacity != '0') { // 找到有的
+                const icon1 = new Icon(cur1), icon2 = new Icon(cur2);
+                exchangeIds(icon1, icon2);
+                exchangeLocations(icon1, icon2); // 交换位置
+                // 动画
+                // icon2.obj.style.transition = 'all 0s';
+                // setIconTransform(icon2, 0, -(icon1 - icon2) / w * 50);
+                // icon2.obj.style.transition = null;
+                // setTimeout(setIconTransform, 50, 0, 0); // 延时执行确保动画播放
+                // 动画
+                changedIcons.push(icon2);
+                cur1 -= w;
+            }
+        }
+        while (cur1 > 0) {
+            getIconByNumId(cur1).remove();
+            spawnIcon(cur1);
+            changedIcons.push(new Icon(cur1));
+            cur1 -= w;
+        }
+    }
+    setTimeout(() => {
+        if (judge(changedIcons)) {
+            setTimeout(fall, 500);
+        }
+    }, 100);
+}
 
+function areBlockAndIconMatch(blockNumId) {
+    return getBlockByNumId(blockNumId).firstChild.id == 'icon' + blockNumId;
 }
